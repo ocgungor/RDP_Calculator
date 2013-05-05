df8db042955acf28a421b990ebec9fa737e08b85
Oguzhan Gungor <cansingungor@gmail.com>
2013-05-06 02:15:55 +1000

    XCode Project added

---
+++ RDP_Calculator/RDP_Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RDP_Calculator
+//
+//  Created by Oguzhan Gungor on 6/05/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
